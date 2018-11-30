11f71fc935e10977b31f4ab095b07114b87e3f33
Greg <gtrestour@gmail.com>
2018-11-30 09:38:41 +0100

    Implementation classe ProdConsBuffer

--- src/jus/poc/prodcons/v1/ProdConsBuffer.java
+++ src/jus/poc/prodcons/v1/ProdConsBuffer.java
@@ -4,23 +4,33 @@ import jus.poc.prodcons.IProdConsBuffer;
 import jus.poc.prodcons.IMessage;
 
 public class ProdConsBuffer implements IProdConsBuffer {
+	
+	private IMessage[] buf = new IMessage[100];
+	public int index = 0;
 
 	@Override
-	public void put(IMessage m) throws InterruptedException {
-		// TODO Auto-generated method stub
-		
+	public synchronized void put(IMessage m) throws InterruptedException {
+		buf[index] = m;
+		index ++;
+		notifyAll();
 	}
 
 	@Override
-	public IMessage get() throws InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
+	public synchronized IMessage get() throws InterruptedException {
+		while(index == 0) {
+            try {
+                //attente passive
+                wait();
+            } catch(InterruptedException ie) {
+                ie.printStackTrace();
+            }
+        }
+        return buf[0];
 	}
 
 	@Override
 	public int nmsg() {
-		// TODO Auto-generated method stub
-		return 0;
+		return index;
 	}
 
 }
